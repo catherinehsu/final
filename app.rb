302c0c2c8367ab2ed6c1b4db78e3214b7c8b7cba
catherinehsu <catherinethsu@gmail.com>
2020-03-14 00:39:47 +0000

    Version 1

--- app.rb
+++ app.rb
@@ -21,6 +21,10 @@ cookclasses_table = DB.from(:cookclasses)
 rsvps_table = DB.from(:rsvps)
 users_table = DB.from(:users)
 
+before do
+    @current_user = users_table.where(id: session["user_id"]).to_a[0]
+end
+
 # enter your Dark Sky API key here
 ForecastIO.api_key = "0329940053e738bed5b2dedd3528c6dd"
 
@@ -56,4 +60,142 @@ get "/" do
     pp @cookclasses
 
     view "cookclasses"
+end
+
+# class details (aka "show")
+get "/cookclasses/:id" do
+    puts "params: #{params}"
+
+    @users_table = users_table
+    @cookclass = cookclasses_table.where(id: params[:id]).to_a[0]
+    pp @cookclass
+
+    @rsvps = rsvps_table.where(cookclasses_id: @cookclass[:id]).to_a
+    @going_count = rsvps_table.where(cookclasses_id: @cookclass[:id], going: true).count
+
+    view "cookclass"
+end
+
+# display the rsvp form (aka "new")
+get "/cookclasses/:id/rsvps/new" do
+    puts "params: #{params}"
+
+    @cookclass = cookclasses_table.where(id: params[:id]).to_a[0]
+    view "new_rsvp"
+end
+
+# receive the submitted rsvp form (aka "create")
+post "/cookclasses/:id/rsvps/create" do
+    puts "params: #{params}"
+
+    # first find the cookclass that rsvp'ing for
+    @cookclass = cookclasses_table.where(id: params[:id]).to_a[0]
+    # next we want to insert a row in the rsvps table with the rsvp form data
+    rsvps_table.insert(
+        cookclasses_id: @cookclass[:id],
+        user_id: session["user_id"],
+        comments: params["comments"],
+        going: params["going"]
+    )
+
+    redirect "/cookclasses/#{@cookclass[:id]}"
+end
+
+# display the rsvp form (aka "edit")
+get "/rsvps/:id/edit" do
+    puts "params: #{params}"
+
+    @rsvp = rsvps_table.where(id: params["id"]).to_a[0]
+    @cookclass = cookclasses_table.where(id: @rsvp[:cookclass_id]).to_a[0]
+    view "edit_rsvp"
+end
+
+# receive the submitted rsvp form (aka "update")
+post "/rsvps/:id/update" do
+    puts "params: #{params}"
+
+    # find the rsvp to update
+    @rsvp = rsvps_table.where(id: params["id"]).to_a[0]
+    # find the rsvp's cookclass
+    @cookclass = cookclasses_table.where(id: @rsvp[:cookclass_id]).to_a[0]
+
+    if @current_user && @current_user[:id] == @rsvp[:id]
+        rsvps_table.where(id: params["id"]).update(
+            going: params["going"],
+            comments: params["comments"]
+        )
+
+        redirect "/cookclasses/#{@cookclass[:id]}"
+    else
+        view "error"
+    end
+end
+
+# delete the rsvp (aka "destroy")
+get "/rsvps/:id/destroy" do
+    puts "params: #{params}"
+
+    rsvp = rsvps_table.where(id: params["id"]).to_a[0]
+    @cookclass = cookclasses_table.where(id: rsvp[:cookclass_id]).to_a[0]
+
+    rsvps_table.where(id: params["id"]).delete
+
+    redirect "/cookclasses/#{@cookclass[:id]}"
+end
+
+# display the signup form (aka "new")
+get "/users/new" do
+    view "new_user"
+end
+
+# receive the submitted signup form (aka "create")
+post "/users/create" do
+    puts "params: #{params}"
+
+    # if there's already a user with this email, skip!
+    existing_user = users_table.where(email: params["email"]).to_a[0]
+    if existing_user
+        view "error"
+    else
+        users_table.insert(
+            name: params["name"],
+            email: params["email"],
+            password: BCrypt::Password.create(params["password"])
+        )
+
+        redirect "/logins/new"
+    end
+end
+
+# display the login form (aka "new")
+get "/logins/new" do
+    view "new_login"
+end
+
+# receive the submitted login form (aka "create")
+post "/logins/create" do
+    puts "params: #{params}"
+
+    # step 1: user with the params["email"] ?
+    @user = users_table.where(email: params["email"]).to_a[0]
+
+    if @user
+        # step 2: if @user, does the encrypted password match?
+        if BCrypt::Password.new(@user[:password]) == params["password"]
+            # set encrypted cookie for logged in user
+            session["user_id"] = @user[:id]
+            redirect "/"
+        else
+            view "create_login_failed"
+        end
+    else
+        view "create_login_failed"
+    end
+end
+
+# logout user
+get "/logout" do
+    # remove encrypted cookie for logged out user
+    session["user_id"] = nil
+    redirect "/logins/new"
 end
--- createdb.rb
+++ createdb.rb
@@ -12,6 +12,9 @@ DB.create_table! :cookclasses do
   String :description, text: true
   String :when
   String :location
+  String :price
+  String :longdescription, text: true
+  String :instructor
 end
 DB.create_table! :rsvps do
   primary_key :id
@@ -33,13 +36,22 @@ cookclasses_table = DB.from(:cookclasses)
 
 cookclasses_table.insert(title: "Oodles of Noodles", 
                     description: "Taiwanese Beef Noodle Soup | Tomato Noodle Soup",
-                    when: "March 14, 6:00 PM (PST)")
+                    when: "March 14, 6:00 PM (PST)",
+                    price: "$70",
+                    instructor: "David Chang",
+                    longdescription: "What better way to beat the cold weather than to warm up with a hearty bowl of noodle soup! This class features two Taiwanese classics that will be your go-to during the winter season. Learn how to make two delicious broths from scratch, which will infuse your noodles with oodles of flavour.")
 
 cookclasses_table.insert(title: "Eggcelent Dishes", 
                     description: "Chinese Tomato and Egg dish | Custard Egg Tarts",
-                    when: "March 21, Noon (PST)")
+                    when: "March 21, Noon (PST)",
+                    price: "$40",
+                    instructor: "Gordon Ramsay",
+                    longdescription: "This class is inspired by my father's love for all things egg! Start off with a simple yet classic dish, and finish off with a dim-sum dessert favorite!")
 
 cookclasses_table.insert(title: "No Meat meet-up", 
                     description: "Stir-fried Chinese Eggplant | Ma-po Tofu",
-                    when: "March 28, 6:00 PM (PST)")
+                    when: "March 28, 6:00 PM (PST)",
+                    price: "$60",
+                    instructor: "Sanne Vloet",
+                    longdescription: "Vegetarians unite! In this class, you'll learn Chinese stir-fry basics that'll open your bellies up to a new world of hot veggie dishes")
 puts "Success!"
